dd6f0ab019e8987ba95a091276484a034e7e789b
Stephen Chen <stephen.chen420@gmail.com>
2024-09-27 09:58:43 -0500

    added test cases for returnCopy

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -4,6 +4,14 @@ public class MyArrays{
 
     System.out.println(arrayToString(new int[]{1, 2, 3}));
     System.out.println(arrayToString(new int[]{1}));
+
+    int[] test = {1, 2, 3, 4};
+    int[] test1 = {1};
+    int[] test2 = {};
+    System.out.println(returnCopy(test) != test);
+    System.out.println(returnCopy(test1) != test1);
+    System.out.println(returnCopy(test2) != test2);
+
   }
 
   public static String arrayToString(int[] nums){
@@ -17,11 +25,11 @@ public class MyArrays{
     return result + "]";
   }
 
-  public static int[] returnCopy(int[]ary){
+  public static int[] returnCopy(int[] ary){
     return new int[0];
   }
 
-  public static int[] concatArray(int[]ary1,int[]ary2){
+  public static int[] concatArray(int[] ary1,int[] ary2){
     return new int[0];
   }
 }
