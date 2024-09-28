636573354744dcb71dd7e50758fb5ca04b596fde
Stephen Chen <stephen.chen420@gmail.com>
2024-09-27 20:20:17 -0400

    finished concatArr

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -8,25 +8,33 @@ public class MyArrays{
 
     //test cases for returnCopy
     int[] test = {1, 2, 3, 4};
-    int[] test1 = {1};
-    int[] test2 = {};
+    int[] test1 = {3, 4, 5};
+    int[] test2 = {6};
+    int[] test3 = {};
 
     int[] test_result = returnCopy(test);
-    System.out.println("Addresses are different: " + test_result != test);
-    System.out.println("Values are the same: " + arrayToString(test).equals(arrayToString(test_result)));
+    System.out.println("Addresses are different: " + (test_result != test));
+    System.out.println("Original Values: " + arrayToString(test) + " Copied Values: " + (arrayToString(test_result)));
     
     int[] test1_result = returnCopy(test1);
-    System.out.println("Addresses are different: " + test1_result != test1);
-    System.out.println("Values are the same: " +arrayToString(test1).equals(arrayToString(test1_result)));
+    System.out.println("Addresses are different: " + (test1_result != test1));
+    System.out.println("Original Values: " + arrayToString(test1) + " Copied Values: " + arrayToString(test1_result));
 
     int[] test2_result = returnCopy(test2);
-    System.out.println("Addresses are different: " + test2_result != test2);
-    System.out.println("Values are the same: " + arrayToString(test2).equals(arrayToString(test2_result)));
+    System.out.println("Addresses are different: " + (test2_result != test2));
+    System.out.println("Original Values: " + arrayToString(test2) + " Copied Values: " + arrayToString(test2_result));
+
+    int[] test3_result = returnCopy(test3);
+    System.out.println("Addresses are different: " + (test3_result != test3));
+    System.out.println("Original Values: " + arrayToString(test3) + " Copied Values: " + arrayToString(test3_result));
 
     //test cases for concatArray
-    //int[] arr1 = 
-    //System.out.println("Expected Value [1, 3, 4, 5, 6, 7, 8, 9], Actual Values: ")
-    //for(int i = 0; i < )
+    System.out.println("Original Arrays : " + arrayToString(test) + " " + arrayToString(test1) + " Concatenated Values: " + arrayToString(concatArray(test, test1)));
+    System.out.println("Original Arrays : " + arrayToString(test) + " " + arrayToString(test2) + " Concatenated Values: " + arrayToString(concatArray(test, test2)));
+    System.out.println("Original Arrays : " + arrayToString(test) + " " + arrayToString(test3) + " Concatenated Values: " + arrayToString(concatArray(test, test3)));
+    System.out.println("Original Arrays : " + arrayToString(test3) + " " + arrayToString(test) + " Concatenated Values: " + arrayToString(concatArray(test3, test)));
+
+
 
   }
 
@@ -50,6 +58,21 @@ public class MyArrays{
   }
 
   public static int[] concatArray(int[] ary1,int[] ary2){
-    return new int[0];
+    int[] combinedArr = new int[ary1.length + ary2.length];
+    int arrIndex = 0;
+    for (int i = 0; i < combinedArr.length; i++){
+      if (i == ary1.length){
+        arrIndex = 0;
+      }
+      if (i >= ary1.length){
+        combinedArr[i] = ary2[arrIndex];
+        arrIndex++;
+      }
+      else{
+        combinedArr[i] = ary1[arrIndex];
+        arrIndex++;
+      }
+    }
+    return combinedArr;
   }
 }
