140f6e6b57f1440d83125cb360f520c49f4030c8
Stephen Chen <stephen.chen420@gmail.com>
2024-10-01 13:23:55 -0500

    finished replaceNegative

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -1,6 +1,6 @@
 // Sean Chen, seanc4194@nycstudents.net, Stephen Chen stephenc88@nycstsudents.net
 public class ArrayMethods{
-  
+
   public static void main(String[] args){
     System.out.println("arrToString");
     System.out.println("Expected: [[1, 2], [4, 5], [6, 7]] Got: " + arrToString(new int[][]{{1,2},{4,5},{6,7}}));
@@ -17,7 +17,7 @@ public class ArrayMethods{
     System.out.println("Expected: 21 Got: " + arr2DSum(new int[][]{{1,2},{},{5,6,7}}));
     System.out.println("Expected: 24 Got: " + arr2DSum(new int[][]{{1,2},{3},{5,6,7}}));
     System.out.println("Expected: 45 Got: " + arr2DSum(new int[][]{{1,2,3},{4,5,6},{7,8,9}}));
-    
+
     System.out.println("swapRC");
     System.out.println("Expected: [[1, 4, 6], [2, 5, 7]] Got: " + arrToString(swapRC(new int[][]{{1,2},{4,5},{6,7}})));
     System.out.println("Expected: [[1, 4], [2, 5], [6, 7]] Got: " + arrToString(swapRC(new int[][]{{1,2,6},{4,5,7}})));
@@ -25,6 +25,9 @@ public class ArrayMethods{
     System.out.println("Expected: [[1, 5, 9], [2, 6, 10], [3, 7, 11], [4, 8, 12]] Got: " + arrToString(swapRC(new int[][]{{1,2,3,4},{5,6,7,8},{9,10,11,12}})));
     System.out.println("Expected: [[1, 2, 3]] Got: " + arrToString(swapRC(new int[][]{{1},{2},{3}})));
     System.out.println("Expected: [[1], [2], [3]] Got: " + arrToString(swapRC(new int[][]{{1, 2, 3}})));
+
+    System.out.println("replaceNegative");
+    System.out.println();
   }
 
   public static String arrToString(int[] nums){
@@ -53,25 +56,56 @@ public class ArrayMethods{
     return result + "]";
   }
 
-public static int arr2DSum(int[][]nums){
-  int sum = 0;
-  for(int i = 0; i < nums.length; i++){
-    for(int j = 0; j < nums[i].length; j++){
-      sum += nums[i][j];
+  public static int arr2DSum(int[][]nums){
+    int sum = 0;
+    for(int i = 0; i < nums.length; i++){
+      for(int j = 0; j < nums[i].length; j++){
+        sum += nums[i][j];
+      }
+    }
+    return sum;
+  }
+
+
+  public static int[][] swapRC(int[][]nums){
+    int[][] newNums = new int[nums[0].length][nums.length];
+
+    for(int i = 0; i < nums[0].length; i++){
+      for(int j = 0; j < nums.length; j++){
+        newNums[i][j] = nums[j][i];
+      }
+    }
+    return newNums;
+  }
+
+
+
+  //3. Modify a given 2D array of integer as follows:
+  //Replace all the negative values:
+  //-When the row number is the same as the column number replace
+  //that negative with the value 1
+  //-All other negatives replace with 0
+  public static void replaceNegative(int[][] vals){
+    for (int i = 0; i < vals.length; i++){
+      for (int j = 0; j < vals[i].length; j++){
+        if (vals[i][j] < 0){
+          if(i == j){
+            vals[i][j] = -1;
+          }
+          else{
+            vals[i][j] = 0;
+          }
+        }
+      }
     }
   }
-  return sum;
-}
 
-
-public static int[][] swapRC(int[][]nums){
-  int[][] newNums = new int[nums[0].length][nums.length];
-
-  for(int i = 0; i < nums[0].length; i++){
-    for(int j = 0; j < nums.length; j++){
-      newNums[i][j] = nums[j][i];
-    }
+  //4. Make a copy of the given 2d array.
+  //When testing : make sure that changing the original does NOT change the copy.
+  //DO NOT use any built in methods that "copy" an array.
+  //You SHOULD write a helper method for this.
+  //If you don't see a good way to do that, you should stop and look at prior methods.
+  public static int[][] copy(int[][] nums){
+    return null;//placeholder so it compiles
   }
-  return newNums;
-}
 }
