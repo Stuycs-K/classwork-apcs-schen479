91f6ff451bc908d061f40c67e3275f47f12b9d8f
Stephen Chen <stephen.chen420@gmail.com>
2024-11-04 10:48:53 -0500

    finished countTrianglesB

--- 10-24-Advent/TriangleTester.java
+++ 10-24-Advent/TriangleTester.java
@@ -6,6 +6,9 @@ class TriangleTester {
     public static void main(String[] args) {
       System.out.println(countTrianglesA("inputTri.txt"));
       System.out.println(countTrianglesB("inputTri.txt"));
+      System.out.println(countTrianglesA("input.txt"));
+      System.out.println(countTrianglesB("input.txt"));
+
     }
 
     public static int countTrianglesA(String filename) {
@@ -15,10 +18,10 @@ class TriangleTester {
         File file = new File(filename);
         Scanner sc = new Scanner(file);
       
-        while(sc.hasNextLine() && sc.hasNextDouble()) {
-          double a = sc.nextDouble();
-          double b = sc.nextDouble();
-          double c = sc.nextDouble();
+        while(sc.hasNextLine() && sc.hasNextInt()) {
+          int a = sc.nextInt();
+          int b = sc.nextInt();
+          int c = sc.nextInt();
           if(testTriangles(a, b, c)){
             sum++;
           }
@@ -31,8 +34,41 @@ class TriangleTester {
       }
     }
 
+    public static int countTrianglesB(String filename){
+      try {
+        File file = new File(filename);
+        Scanner sc = new Scanner(file);
+          
+        int sum = 0;
+        while(sc.hasNextLine() && sc.hasNextInt()) {
+          int a0 = sc.nextInt();
+          int b0 = sc.nextInt();
+          int c0 = sc.nextInt();
+          int a1 = sc.nextInt();
+          int b1 = sc.nextInt();
+          int c1 = sc.nextInt();
+          int a2 = sc.nextInt();
+          int b2 = sc.nextInt();
+          int c2 = sc.nextInt();
+          if(testTriangles(a0, a1, a2)){
+            sum++;
+          }
+          if(testTriangles(b0, b1, b2)){
+            sum++;
+          }
+          if(testTriangles(c0, c1, c2)){
+            sum++;
+          }
+        }
+        return sum; 
+      }
+      catch (FileNotFoundException ex) {
+        System.out.println("File not found");
+        return -1; 
+      }    
+    }
 
-    public static boolean testTriangles(double a, double b, double c){
+    public static boolean testTriangles(int a, int b, int c){
       return ((a+b>c)&&(c+b>a)&&(a+c>b));
     }
   }
