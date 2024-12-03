54a0e4c15b97dbd06557f98c652967db83d7a544
Stephen Chen <stephen.chen420@gmail.com>
2024-12-03 11:16:40 -0500

    adding work from class

---
+++ 2024-12-03-colors/ColorDemo.java
@@ -0,0 +1,12 @@
+public class ColorDemo{
+  public static final String RESET = "u\001b[0m";
+  public static final String CLEAR = "u\001b[2]";
+
+  public static void main(String[] args){
+    System.out.print("\u001b[31mWelcome ");
+    System.out.print("\u001b[34mTo ");
+    System.out.print("\u001b[36mMy Demo");
+
+    for (int i = 0; i < )
+  }
+}
