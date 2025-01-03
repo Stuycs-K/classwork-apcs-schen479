b2460aadbd8f82f4ff9542fad6aec13a19f08691
Stephen Chen <stephen.chen420@gmail.com>
2025-01-03 10:57:02 -0500

    finished makeWords

--- 2025-01-02/Driver.java
+++ 2025-01-02/Driver.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
+
 public class Driver{
   public static void main(String[] args){
-    System.out.println(fibonacci(46));
+    makeWords(3, "", "abc");
+    makeWords(4, "", "abcd");
+
   }
 
   public static int fibonacci(int n){
@@ -12,6 +16,17 @@ public class Driver{
     }
     else{
       return fibonacci(n - 2) + fibonacci (n - 1);
+    }
+  }
+
+  public static void makeWords(int remainingLetters, String result, String alphabet){
+    if (remainingLetters == 0){
+      System.out.println(result);
+    }
+    else{
+      for (int i = 0; i < alphabet.length(); i++){
+        makeWords(remainingLetters - 1, result + alphabet.charAt(i), alphabet);
+      }
     }
   }
 }
